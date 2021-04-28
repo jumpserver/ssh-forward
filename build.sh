e88ab3d2c58182d1b2ad88f625950b9a18f09544
ibuler <ibuler@qq.com>
2021-04-28 15:13:56 +0800

    perf: 优化build

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ echo "rm -rf $BUILD_DIR/*"
 rm -rf $BUILD_DIR/*
 
 for os in darwin linux;do
+  for arch in amd64 arm64;do
     export GOOS=$os
-    export GOARCH=amd64
+    export GOARCH=$arch
     echo "go build -o $BUILD_DIR/$BIN_NAME"
     go build -o $BUILD_DIR/$BIN_NAME
-    cd $BUILD_DIR
-    tar czf $GOOS-$GOARCH.tar.gz $BIN_NAME
-    cd -
+    cd $BUILD_DIR && tar czf $GOOS-$GOARCH.tar.gz $BIN_NAME && cd - || return
+  done
 done
---
+++ go.mod
@@ -0,0 +1,8 @@
+module ssh-forward
+
+go 1.14
+
+require (
+	github.com/sevlyar/go-daemon v0.1.5
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b
+)
