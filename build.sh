3c1f82bdfb165e58a0c6b14363bd4d65a80875b4
ibuler <ibuler@qq.com>
2021-07-20 18:49:13 +0800

    perf: 更新构建脚本

--- build.sh
+++ build.sh
@@ -6,12 +6,13 @@ BIN_NAME="ssh-forward"
 echo "rm -rf $BUILD_DIR/*"
 rm -rf $BUILD_DIR/*
 
+export CGO_ENABLED=0
 for os in darwin linux;do
   for arch in amd64 arm64;do
     export GOOS=$os
     export GOARCH=$arch
     echo "go build -o $BUILD_DIR/$BIN_NAME"
     go build -o $BUILD_DIR/$BIN_NAME
-    cd $BUILD_DIR && tar czf $GOOS-$GOARCH.tar.gz $BIN_NAME && cd - || return
+    cd $BUILD_DIR && tar czf ssh-forward-$GOOS-$GOARCH.tar.gz $BIN_NAME && cd - || return
   done
 done
